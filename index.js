baf51eda122be5b6fbab2848a3f75ceb27fe154e
KhanhLy1401 <khanhlypt14@gmail.com>
2024-05-25 13:49:23 +0700

    nhúng boostrap

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+require("dotenv").config();
 
 const route = require("./routes/client/index.route")
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+app.use(express.static("public"));
 
 route(app);
 
