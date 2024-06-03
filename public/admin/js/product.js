c64654b4c66d81a146db60eb8fa6485b2fdce9ad
KhanhLy1401 <154037682+KhanhLy1401@users.noreply.github.com>
2024-06-03 17:50:29 +0700

    Xóa vĩnh viễn

--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -16,11 +16,33 @@ if(buttonsChangeStatus.length > 0){
             console.log(statusChange);
             // Khi nhấn vào thì thêm path mới
             const action = path + `/${statusChange}/${id}?_method=PATCH`;
-            console.log(action);
             
             formChangeStatus.action = action;
 
             formChangeStatus.submit();
         });
     });
+}
+
+//End Change Status
+
+//Delete Item
+const buttonsDelete = document.querySelectorAll("[button-delete]");
+if(buttonsDelete.length > 0){ // Check xem nếu tồn tại buttonsDelete
+    
+    const formDeleteItem = document.querySelector("#form-delete-item");
+    const path = formDeleteItem.getAttribute("data-path");
+
+    buttonsDelete.forEach(button => { // Lọc qua từng button bắt sự kiên click
+        button.addEventListener("click", ()=>{
+            const isConirm = confirm("Bạn có chắc muốn xóa sản phẩm này không?");
+            if(isConirm) {
+                const id = button.getAttribute("data-id");
+
+                const action = `${path}/${id}?_method=DELETE`;
+                formDeleteItem.action = action;
+                formDeleteItem.submit();
+            }
+        })
+    })
 }
