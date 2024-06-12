e6d4c6bdbd83e6ee2374c178cd4691037f241365
KhanhLy1401 <154037682+KhanhLy1401@users.noreply.github.com>
2024-06-12 21:28:12 +0700

    Validate dữ liệu

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -155,6 +155,7 @@ module.exports.create = async (req, res)=> {
 
 //[POST] /admin/products/create
 module.exports.createPost = async (req, res)=> {
+    
     req.body.price=parseInt(req.body.price);
     req.body.discountPercentage=parseInt(req.body.discountPercentage);
     req.body.stock=parseInt(req.body.stock);
@@ -165,7 +166,10 @@ module.exports.createPost = async (req, res)=> {
         req.body.position = parseInt(req.body.position);
     }
 
+    if(req.file){
     req.body.thumbnail = `/uploads/${req.file.filename}`;
+        
+    }
     const product = new Product(req.body);
     await product.save();
     res.redirect(`${systemConfig.prefixAdmin}/products`)
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -3,8 +3,9 @@ const multer = require("multer");
 const router = express.Router();
 const upload = multer({dest: "./public/uploads/"})
 
-const controller = require("../../controllers/admin/product.controller");
 
+const controller = require("../../controllers/admin/product.controller");
+const validate = require("../../validate/admin/product.validate")
 // khi /product thì sẽ gọi đến controller product.controller ở admin
 router.get("/", controller.index)
 
@@ -16,7 +17,12 @@ router.delete("/delete/:id", controller.deleteItem);
 
 router.get("/create", controller.create);
 
-router.post("/create", upload.single('thumbnail'), controller.createPost);
+router.post(
+    "/create", 
+    upload.single('thumbnail'),
+    validate.createPost, 
+    controller.createPost
+);
 
 
 
