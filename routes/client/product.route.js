f43fb46285265a14b0871b07bed6360da4f5818b
KhanhLy1401 <154037682+KhanhLy1401@users.noreply.github.com>
2024-06-13 01:55:38 +0700

    Trang chi tiết client

--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -5,4 +5,7 @@ const controller = require("../../controllers/client/product.controller")
 
 //Từ controller gọi đến hàm index
 router.get("/products", controller.index);
+
+router.get("/products/:slug", controller.detail);
+
 module.exports = router;
