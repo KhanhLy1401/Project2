562c7e622e6a5a6be587460625f65d8c27ca9c51
KhanhLy1401 <154037682+KhanhLy1401@users.noreply.github.com>
2024-06-26 12:23:55 +0700

    Sửa nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,6 +1,7 @@
 const Role = require("../../models/role.model");
 const systemConfig = require("../../config/system");
 
+
 module.exports.index = async (req, res) => {
     let find = {
         deleted: false
@@ -30,4 +31,44 @@ module.exports.createPost = async (req, res) => {
     await record.save();
     res.redirect(`${systemConfig.prefixAdmin}/roles`);
     
+}
+
+//[GET]/admin/roles/edit
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        let find = {
+            _id: id,
+            deleted: false
+        }
+    
+        const data = await Role.findOne(find);
+    
+        res.render("admin/pages/roles/edit", {
+            pageTitle: "Sửa nhóm quyền",
+            data: data
+        });
+    } catch(error){
+        res.redirect(`${systemConfig.prefixAdmin}/roles`);
+    }
+}
+
+module.exports.editPatch = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        let find = {
+            _id: id,
+            deleted: false
+        }
+    
+        await Role.updateOne({ _id: id}, req.body);
+    
+        res.flash("success", "Cập nhật nhóm quyền thành công !")
+    } catch(error) {
+        req.flash("error", "Cập nhật thất bại")
+    }
+    res.redirect("back");
+
 }
