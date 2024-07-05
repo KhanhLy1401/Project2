ec2168e9e0b257e705beabe36ed7a08c67b082bf
KhanhLy1401 <154037682+KhanhLy1401@users.noreply.github.com>
2024-07-05 11:33:34 +0700

    Tính năng đăng nhập

---
+++ controllers/admin/auth.controller.js
@@ -0,0 +1,45 @@
+const md5 = require("md5");
+const Account = require("../../models/account.model");
+const systemConfig = require("../../config/system");
+
+//[GET] /admin/auth/login
+module.exports.login = async (req, res) => {
+    
+    res.render("admin/pages/auth/login", {
+        pageTitle: "Trang đăng nhập",
+    })
+}
+
+//[POST] /admin/auth/login
+module.exports.loginPost = async (req, res) => {
+    console.log(req.body);
+    const email = req.body.email;
+    const password = req.body.password;
+    const user = await Account.findOne({
+        email: email,
+        deleted: false
+    });
+    if(!user) {
+        req.flash("error", "Email không tồn tại");
+        res.redirect("back");
+        return;
+    }
+
+    if(md5(password) != user.password) {
+        req.flash("error", " Sai mật khẩu");
+        res.redirect("back");
+        return;
+    }
+
+    if(user.status != "active") {
+        req.flash("error", " Tài khoản đã bị khóa");
+        res.redirect("back");
+        return;
+    }
+
+    res.cookie("token", user.token);
+    res.redirect(`/admin/dashboard`);
+    
+}
+
+// đăng nhập thành công lưu token vào cookie 
