a12c32115f4cf927efdc7f410a07a09d347c0292
KhanhLy1401 <154037682+KhanhLy1401@users.noreply.github.com>
2024-07-06 12:04:21 +0700

    Hiển thị menu chung bên clinet

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,7 +1,17 @@
+const ProductsCategory = require("../../models/products-category.model")
+const  createTreeHelper = require("../../helpers/createTree")
+
 // [GET]/
 
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    const productsCategory = await ProductsCategory.find({
+        deleted: false
+    });
+
+    const newProductsCategory = createTreeHelper.tree(productsCategory);
+
     res.render("client/pages/home/index", {
-        pageTitle: "Trang chủ"
+        pageTitle: "Trang chủ",
+        layoutProductsCategory: newProductsCategory
     });
 }
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,11 @@
+const categoryMiddleware = require("../../middlewares/client/category.middleware")
+
 //Lấy biến app từ index.js truyền sang
 const productRoutes = require("./product.route");
 const homeRoutes = require("./home.route");
 module.exports = (app) => {
+    app.use(categoryMiddleware.category);
+
     app.use("/", homeRoutes);
 
     app.use("/", productRoutes);
