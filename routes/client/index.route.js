b312d6525955e55ae56b4f6fcaef3c1a6a877639
KhanhLy1401 <154037682+KhanhLy1401@users.noreply.github.com>
2024-07-08 17:19:59 +0700

    cardId

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,14 +1,21 @@
 const categoryMiddleware = require("../../middlewares/client/category.middleware")
 
+const cartMiddleware= require("../../middlewares/client/cart.middleware")
+
 //Lấy biến app từ index.js truyền sang
 const productRoutes = require("./product.route");
 const homeRoutes = require("./home.route");
 const searchRoutes = require("./search.route");
+const cartRoutes = require("./cart.route");
 module.exports = (app) => {
     app.use(categoryMiddleware.category);
+    app.use(cartMiddleware.cartId);
 
     app.use("/", homeRoutes);
 
     app.use("/products", productRoutes);
     app.use("/search", searchRoutes);
+    app.use("/cart", cartRoutes);
+
+
 }
