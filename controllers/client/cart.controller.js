edef361ba42f5b6187ccbbb5e82d9a690c1ff395
KhanhLy1401 <154037682+KhanhLy1401@users.noreply.github.com>
2024-07-09 00:29:39 +0700

    Cập nhật số lượng giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -85,3 +85,22 @@ module.exports.delete = async (req, res )=> {
     req.flash("success", "Đã xóa sản phẩm khỏi giỏ hàng");
     res.redirect("back");
 }
+
+//[GET] /cart/update/:productId
+
+module.exports.update = async (req, res )=> {
+    const cartId = req.cookies.cartId;
+    const productId = req.params.productId;
+    const quantity = req.params.quantity;
+
+    await Cart.updateOne(
+        {
+            _id: cartId,
+            "products.product_id": productId
+        }, 
+        {
+            'products.$.quantity': quantity
+        })
+    // req.flash("success", "Đã cập nhật ");
+    res.redirect("back");
+}
