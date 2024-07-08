71aa1c01b3d3ee3122cbbd87361016d3b7e9debc
KhanhLy1401 <154037682+KhanhLy1401@users.noreply.github.com>
2024-07-08 19:00:59 +0700

    Trang giỏ hàng

--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -5,4 +5,6 @@ const controller = require("../../controllers/client/cart.controller")
 
 //Từ controller gọi đến hàm index
 router.post("/add/:productId", controller.addPost);
+
+router.get("/", controller.index);
 module.exports = router;
