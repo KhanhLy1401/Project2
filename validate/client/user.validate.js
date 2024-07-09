4603ce75e9ed5c95e9e879afe958508dbeea5253
KhanhLy1401 <154037682+KhanhLy1401@users.noreply.github.com>
2024-07-09 08:59:29 +0700

    Đăng ký

---
+++ validate/client/user.validate.js
@@ -0,0 +1,21 @@
+module.exports.registerPost = (req, res, next) => {
+    if(!req.body.fullName){
+        req.flash("error", 'Vui lòng nhập họ tên!');
+        res.redirect("back");
+        return;
+    }
+
+    if(!req.body.email){
+        req.flash("error", 'Vui lòng nhập email!');
+        res.redirect("back");
+        return;
+    }
+
+    if(req.body.password < 8){
+        req.flash("error", 'Vui lòng nhập mật khẩu ít nhất 8 kí tự!');
+        res.redirect("back");
+        return;
+    }
+
+    next();
+}
